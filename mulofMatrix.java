7a8c8efaaf3953698a087a7da87f7066983c5ce9
REECARD <reecarddbc@gmail.com>
2026-07-24 08:05:37 +0530

    Create mulofMatrix.java

---
+++ mulofMatrix.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] A = new int[3][3];
+        int[][] B = new int[3][3];
+        int[][] C = new int[3][3];
+
+        System.out.println("Enter elements of Matrix A:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter elements of Matrix B:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+
+        // Matrix Multiplication
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                C[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        System.out.println("Resultant Matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
